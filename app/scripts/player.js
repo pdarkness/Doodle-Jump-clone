a59bfc1cf773b20b1fcf04be8e241f18277b4ce7
pdarkness <kntur11@ru.is>
2013-08-29 00:50:12 +0000

    O gravity aren't thou a cold hearted bitch

--- app/scripts/player.js
+++ app/scripts/player.js
@@ -2,37 +2,40 @@
 
 define(['controls'], function(controls) {
 
-  var PLAYER_SPEED = 350;
+    var PLAYER_SPEED = 350;
+    var GRAVITY = 5000;
 
-  var Player = function(el) {
+    var Player = function(el) {
     this.el = el;
     this.pos = { x: 0, y: 0 };
     this.vel = { x: 0, y: 0 };
-  };
+};
 
-  Player.prototype.onFrame = function(delta) {
+Player.prototype.onFrame = function(delta) {
     // Player input
     if (controls.keys.right) {
-      this.vel.x = PLAYER_SPEED;
+    this.vel.x = PLAYER_SPEED;
     } else if (controls.keys.left) {
-      this.vel.x = -PLAYER_SPEED;
+    this.vel.x = -PLAYER_SPEED;
     } else {
-      this.vel.x = 0;
+    this.vel.x = 0;
     }
     if (controls.keys.up) {
-        this.vel.y = -PLAYER_SPEED
+    this.vel.y = -PLAYER_SPEED
     } else if (controls.keys.down) {
-        this.vel.y = PLAYER_SPEED
+    this.vel.y = PLAYER_SPEED
     } else {
-        this.vel.y = 0;
-    }
+    this.vel.y = 0;
+}
 
-    this.pos.x += delta * this.vel.x;
-    this.pos.y += delta * this.vel.y;
+this.vel.y += GRAVITY * delta;
+
+this.pos.x += delta * this.vel.x;
+this.pos.y += delta * this.vel.y;
 
 
     // Update UI
     this.el.css('transform', 'translate3d(' + this.pos.x + 'px,' + this.pos.y + 'px,0)');
-  };
-  return Player;
+};
+    return Player;
 });
